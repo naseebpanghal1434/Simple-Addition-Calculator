9f909dc811a8835bad7f5fe52933a051b29f7d61
Naseeb Panghal <naseebsingh3510@gmail.com>
2022-08-26 22:53:57 +0530

    Added Sub, Mul, Div functions

--- script.js
+++ script.js
@@ -8,6 +8,31 @@ function addNumbers() {
 
 }
 
+function subNumbers() {
+    let num1 = parseInt(document.getElementById("firstNum").value);
+    let num2 = parseInt(document.getElementById("secondNum").value);
+
+    let result = num1 - num2;
+    document.getElementById("showResult").value = result;
+
+}
+function mulNumbers() {
+    let num1 = parseInt(document.getElementById("firstNum").value);
+    let num2 = parseInt(document.getElementById("secondNum").value);
+
+    let result = num1 * num2;
+    document.getElementById("showResult").value = result;
+
+}
+function divNumbers() {
+    let num1 = parseInt(document.getElementById("firstNum").value);
+    let num2 = parseInt(document.getElementById("secondNum").value);
+
+    let result = num1 / num2;
+    document.getElementById("showResult").value = result;
+
+}
+
 //reset button function to reset all the values 
 function clearAllFields() {
     document.querySelector("#firstNum").value = '';
