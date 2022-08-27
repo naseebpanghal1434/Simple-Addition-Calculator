2b579c6c29bf8d2c3f5a9e6cd2dae10ef8fe1e30
Naseeb Panghal <naseebsingh3510@gmail.com>
2022-08-27 08:39:26 +0530

    Select Operations implemented and optimisation

--- script.js
+++ script.js
@@ -1,38 +1,3 @@
-// addNumbers function to add the two numbers
-function addNumbers() {
-    let num1 = parseInt(document.getElementById("firstNum").value);
-    let num2 = parseInt(document.getElementById("secondNum").value);
-
-    let result = num1 + num2;
-    document.getElementById("showResult").value = result;
-
-}
-
-function subNumbers() {
-    let num1 = parseInt(document.getElementById("firstNum").value);
-    let num2 = parseInt(document.getElementById("secondNum").value);
-
-    let result = num1 - num2;
-    document.getElementById("showResult").value = result;
-
-}
-function mulNumbers() {
-    let num1 = parseInt(document.getElementById("firstNum").value);
-    let num2 = parseInt(document.getElementById("secondNum").value);
-
-    let result = num1 * num2;
-    document.getElementById("showResult").value = result;
-
-}
-function divNumbers() {
-    let num1 = parseInt(document.getElementById("firstNum").value);
-    let num2 = parseInt(document.getElementById("secondNum").value);
-
-    let result = num1 / num2;
-    document.getElementById("showResult").value = result;
-
-}
-
 //reset button function to reset all the values 
 function clearAllFields() {
     document.querySelector("#firstNum").value = '';
@@ -61,3 +26,87 @@ function changeBackground() {
         clicked = true;
     }
 }
+
+
+// Modified/Optimised Code for Operations 
+
+function doOp(){
+let selectedOperation = document.querySelector("#forOperationsId").value;
+
+let num1 = parseInt(document.getElementById("firstNum").value);
+let num2 = parseInt(document.getElementById("secondNum").value);
+
+
+    switch(selectedOperation){
+        case "add": {
+            let result = num1 + num2;
+            document.getElementById("showResult").value = result;
+            break;
+
+        }
+        case "sub": {
+            let result = num1 - num2;
+            document.getElementById("showResult").value = result;
+            break;
+
+        }
+        case "mul": {
+            let result = num1 * num2;
+            document.getElementById("showResult").value = result;
+            break;
+
+        }
+        case "div": {
+            let result = num1 / num2;
+            document.getElementById("showResult").value = result;
+            break;
+        }
+
+        default: {
+            console.log("Select Correct Option");
+        }
+
+    }
+}
+
+
+//** Function which are used to perform multiple operations individually.  */
+
+// addNumbers function to add the two numbers
+
+/* function addNumbers() {
+    let num1 = parseInt(document.getElementById("firstNum").value);
+    let num2 = parseInt(document.getElementById("secondNum").value);
+
+    let result = num1 + num2;
+    document.getElementById("showResult").value = result;
+
+} */
+
+/* function subNumbers() {
+    let num1 = parseInt(document.getElementById("firstNum").value);
+    let num2 = parseInt(document.getElementById("secondNum").value);
+
+    let result = num1 - num2;
+    document.getElementById("showResult").value = result;
+
+} */
+
+/* function mulNumbers() {
+    let num1 = parseInt(document.getElementById("firstNum").value);
+    let num2 = parseInt(document.getElementById("secondNum").value);
+
+    let result = num1 * num2;
+    document.getElementById("showResult").value = result;
+
+} */
+
+
+/* function divNumbers() {
+    let num1 = parseInt(document.getElementById("firstNum").value);
+    let num2 = parseInt(document.getElementById("secondNum").value);
+
+    let result = num1 / num2;
+    document.getElementById("showResult").value = result;
+
+} */
